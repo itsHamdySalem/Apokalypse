8a6bb74118b7437547ef30d47e88e52145fc4a4b
itsHamdySalem <hamdysalem503@gmail.com>
2024-05-11 03:03:05 +0300

    Adding light system;

--- source/common/components/light.hpp
+++ source/common/components/light.hpp
@@ -1,35 +1,17 @@
 #pragma once
 
 #include "../ecs/component.hpp"
-#include "../ecs/transform.hpp"
 
 #include <glm/glm.hpp>
-#include <stdexcept>
-#include <string>
 
-namespace our {
+namespace our
+{
 
-    enum class LightType {
-        DIRECTIONAL,
-        POINT,
-        SPOT
-    };
-
-    class LightComponent : public Component {
+    class LightComponent : public Component
+    {
     public:
-        // Constants for default values
-        static constexpr glm::vec3 DEFAULT_COLOR {1.0f, 1.0f, 1.0f};
-        static constexpr glm::vec3 DEFAULT_DIFFUSE {1.0f, 1.0f, 1.0f};
-        static constexpr glm::vec3 DEFAULT_SPECULAR {1.0f, 1.0f, 1.0f};
-        static constexpr glm::vec3 DEFAULT_DIRECTION {1.0f, 1.0f, 1.0f};
-        static constexpr glm::vec3 DEFAULT_ATTENUATION {1.0f, 0.0f, 0.0f};
-        static constexpr glm::vec2 DEFAULT_CONE_ANGLES {glm::radians(10.0f), glm::radians(80.0f)};
-
-        int lightType;
+        int kind;
         glm::vec3 color;
-        glm::vec3 diffuse;
-        glm::vec3 specular;
-        glm::vec3 direction;
         glm::vec3 attenuation;
         glm::vec2 cone_angles;
 
--- source/common/ecs/world.hpp
+++ source/common/ecs/world.hpp
@@ -3,75 +3,98 @@
 #include <unordered_set>
 #include "entity.hpp"
 
-namespace our {
+namespace our
+{
+    // Light Struct to store light info
+    struct Light
+    {
+        int kind;
+        glm::vec3 attenuation;
+        glm::vec3 color;
+        glm::vec2 cone_angles;
+        glm::vec3 direction;
+        glm::vec3 position;
+    };
 
     // This class holds a set of entities
-    class World {
-        std::unordered_set<Entity*> entities; // These are the entities held by this world
-        std::unordered_set<Entity*> markedForRemoval; // These are the entities that are awaiting to be deleted
-                                                      // when deleteMarkedEntities is called
+    class World
+    {
+        std::unordered_set<Entity *> entities;         // These are the entities held by this world
+        std::unordered_set<Entity *> markedForRemoval; // These are the entities that are awaiting to be deleted
+                                                       // when deleteMarkedEntities is called
     public:
+        Light lights[16];
+        int light_count = 0;
 
         World() = default;
 
         // This will deserialize a json array of entities and add the new entities to the current world
         // If parent pointer is not null, the new entities will be have their parent set to that given pointer
         // If any of the entities has children, this function will be called recursively for these children
-        void deserialize(const nlohmann::json& data, Entity* parent = nullptr);
+        void deserialize(const nlohmann::json &data, Entity *parent = nullptr);
 
         // This adds an entity to the entities set and returns a pointer to that entity
         // WARNING The entity is owned by this world so don't use "delete" to delete it, instead, call "markForRemoval"
         // to put it in the "markedForRemoval" set. The elements in the "markedForRemoval" set will be removed and
         // deleted when "deleteMarkedEntities" is called.
-        Entity* add() {
-            //TODO (DONE): (Req 8) Create a new entity, set its world member variable to this,
-            // and don't forget to insert it in the suitable container.
-            Entity* nwEntity = new Entity();
+        Entity *add()
+        {
+            // TODO (DONE): (Req 8) Create a new entity, set its world member variable to this,
+            //  and don't forget to insert it in the suitable container.
+            Entity *nwEntity = new Entity();
             nwEntity->world = this;
             entities.insert(nwEntity);
             return nwEntity;
         }
 
         // This returns and immutable reference to the set of all entites in the world.
-        const std::unordered_set<Entity*>& getEntities() {
+        const std::unordered_set<Entity *> &getEntities()
+        {
             return entities;
         }
 
         // This marks an entity for removal by adding it to the "markedForRemoval" set.
         // The elements in the "markedForRemoval" set will be removed and deleted when "deleteMarkedEntities" is called.
-        void markForRemoval(Entity* entity){
-            //TODO (DONE): (Req 8) If the entity is in this world, add it to the "markedForRemoval" set.
-            if (entity->world == this) {
+        void markForRemoval(Entity *entity)
+        {
+            // TODO (DONE): (Req 8) If the entity is in this world, add it to the "markedForRemoval" set.
+            if (entity->world == this)
+            {
                 markedForRemoval.insert(entity);
             }
         }
 
         // This removes the elements in "markedForRemoval" from the "entities" set.
         // Then each of these elements are deleted.
-        void deleteMarkedEntities(){
-            //TODO (DONE): (Req 8) Remove and delete all the entities that have been marked for removal
-            for (auto &en : markedForRemoval) {
+        void deleteMarkedEntities()
+        {
+            // TODO (DONE): (Req 8) Remove and delete all the entities that have been marked for removal
+            for (auto &en : markedForRemoval)
+            {
                 entities.erase(en);
                 delete en;
             }
             markedForRemoval.clear();
         }
 
-        //This deletes all entities in the world
-        void clear(){
-            //TODO (DONE): (Req 8) Delete all the entities and make sure that the containers are empty
-            for (auto &en : entities) delete en;
+        // This deletes all entities in the world
+        void clear()
+        {
+            // TODO (DONE): (Req 8) Delete all the entities and make sure that the containers are empty
+            for (auto &en : entities)
+                delete en;
             entities.clear();
             markedForRemoval.clear();
         }
 
-        //Since the world owns all of its entities, they should be deleted alongside it.
-        ~World(){
+        // Since the world owns all of its entities, they should be deleted alongside it.
+        ~World()
+        {
             clear();
         }
 
         // The world should not be copyable
-        World(const World&) = delete;
+        World(const World &) = delete;
         World &operator=(World const &) = delete;
     };
 
